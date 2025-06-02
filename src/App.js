43e8dd48f00ed8b0204a1d554c8242a959d8cc05
crispyvicky <ankam.vignesh123@gmail.com>
2025-06-02 17:07:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+// App.js
+
 import './App.css';
+import SpeedTypingGame from './components/SpeedTypingGame';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>  VICKY SPEED TYPING GAME</h1>
+      <SpeedTypingGame />
     </div>
   );
 }
